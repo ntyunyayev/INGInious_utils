cd5eb7ea475ab553f7f84234c872f7610c15284c
Nikita Tyunyayev <nikita.tyunyayev@uclouvain.be>
2024-02-26 11:04:08 +0100

    preparing helper to change all deadlines

---
+++ change_helper.sh
@@ -0,0 +1,4 @@
+python3 change_deadline.py --course_folder TMP/lsinc1102-inginious --exercises_id "assembleur" --new_deadline "//2024-03-01 12:00:00"
+python3 change_deadline.py --course_folder TMP/lsinc1102-inginious --exercises_id "s2" --new_deadline "//2024-03-07 12:00:00"
+python3 change_deadline.py --course_folder TMP/lsinc1102-inginious --exercises_id "s3_assembleur_tableaux_chanes_de_charactres" --new_deadline "//2024-03-14 12:00:00"
+python3 change_deadline.py --course_folder TMP/lsinc1102-inginious --exercises_id "s4_assembleur_fonctions_procdures" --new_deadline "//2024-03-14 12:00:00"
